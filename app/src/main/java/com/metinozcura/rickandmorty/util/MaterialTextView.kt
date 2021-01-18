903a5da4215a81f37f886ef054b3716743564abd
Metin <metinozcura@gmail.com>
2021-01-18 21:28:01 +0300

    MaterialTextView setDrawableTint fix.

--- app/src/main/java/com/metinozcura/rickandmorty/util/MaterialTextView.kt
+++ app/src/main/java/com/metinozcura/rickandmorty/util/MaterialTextView.kt
@@ -4,5 +4,7 @@ import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import com.google.android.material.textview.MaterialTextView
 
-fun MaterialTextView.setDrawableLeft(@ColorRes res: Int) =
+fun MaterialTextView.setDrawableLeft(@ColorRes res: Int) {
+    if (compoundDrawables[0] == null) return
     compoundDrawables[0].setTint(ContextCompat.getColor(context, res))
+}
