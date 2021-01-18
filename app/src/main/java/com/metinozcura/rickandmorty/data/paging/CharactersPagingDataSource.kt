f82a6e2ca8baec149a41bc6f0f6a9ac63fdf9e96
Metin <metinozcura@gmail.com>
2021-01-18 21:27:20 +0300

    Next page number query.

--- app/src/main/java/com/metinozcura/rickandmorty/data/paging/CharactersPagingDataSource.kt
+++ app/src/main/java/com/metinozcura/rickandmorty/data/paging/CharactersPagingDataSource.kt
@@ -1,5 +1,6 @@
 package com.metinozcura.rickandmorty.data.paging
 
+import android.net.Uri
 import androidx.paging.PagingSource
 import com.metinozcura.rickandmorty.data.model.Character
 import com.metinozcura.rickandmorty.data.service.CharacterApi
@@ -7,15 +8,19 @@ import javax.inject.Inject
 
 class CharactersPagingDataSource @Inject constructor(val service: CharacterApi) : PagingSource<Int, Character>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> {
-        val nextPageNumber = params.key ?: 1
+        val pageNumber = params.key ?: 1
         return try {
-            val response = service.getAllCharacters(nextPageNumber)
+            val response = service.getAllCharacters(pageNumber)
             val pagedResponse = response.body()
             val data = pagedResponse?.results
+
+            val uri = Uri.parse(pagedResponse?.pageInfo?.next)
+            val nextPageNumber = uri.getQueryParameter("page")?.toInt()
+
             LoadResult.Page(
                 data = data.orEmpty(),
                 prevKey = null,
-                nextKey = nextPageNumber.inc()
+                nextKey = nextPageNumber
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
